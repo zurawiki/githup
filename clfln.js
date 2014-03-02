5b30ceaa6612f46a7732dd4168abc4b7f7f58872
rzurawicki <rzurawicki@gmail.com>
2014-03-01 20:12:20 -0500

    Initial # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # On branch master # # Initial commit # # Changes to be committed: #   (use "git rm --cached <file>..." to unstage) # #	new file:   GitHub.html #	new file:   GitHub_files/206afa393d101c950c6d9836a7faa61c #	new file:   GitHub_files/33e009132b1bc3c5bd7be7e0762d4c46 #	new file:   GitHub_files/3913dc7bc4633553e8efce9122f587a4 #	new file:   GitHub_files/3913dc7bc4633553e8efce9122f587a4(1) #	new file:   GitHub_files/40bf66f430424fc205e6334db6d59279 #	new file:   GitHub_files/40bf66f430424fc205e6334db6d59279(1) #	new file:   GitHub_files/40bf66f430424fc205e6334db6d59279(2) #	new file:   GitHub_files/41b0fed2e9862408cb354ba68c01a357 #	new file:   GitHub_files/5d80c2b34c40d90ab0ed96337dd993c1 #	new file:   GitHub_files/7ac0928bbcb3767bf4e086904890c1b5 #	new file:   GitHub_files/9b939f13d5e04aa97a2ea68efb30af68 #	new file:   GitHub_files/9ea85ee8d441e1fc9c33ea5ff9d5bf7d #	new file:   GitHub_files/a6f2972558b67d22fb58ccaaaca87250 #	new file:   GitHub_files/a6f2972558b67d22fb58ccaaaca87250(1) #	new file:   GitHub_files/api.js #	new file:   GitHub_files/b0be173e35c78d0771bfb39ec954a144 #	new file:   GitHub_files/c058fe7305d4fe078b3f0a8fde35aced #	new file:   GitHub_files/c058fe7305d4fe078b3f0a8fde35aced(1) #	new file:   GitHub_files/cf762d27a6ba2c472fbaad3988360d70 #	new file:   GitHub_files/cf762d27a6ba2c472fbaad3988360d70(1) #	new file:   GitHub_files/frameworks-693e11922dcacc3a7408a911fe1647da4febd3bd.js #	new file:   GitHub_files/ga.js #	new file:   GitHub_files/github-9a2bd2ef14857c7ba70e7f1b96f8021d89fa7439.css #	new file:   GitHub_files/github-df91b5db3eea6cebc49f29b84a77174f8e21a8a9.js #	new file:   GitHub_files/github2-d0c84fb768ff582acb2168e18b8ae157fff086fc.css #

---
+++ clfln.js
@@ -0,0 +1,57 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var my_http = require("http");  
+
+
+var data = [];
+
+var url = 'http://www.commitlogsfromlastnight.com/';
+request(url, function(err, resp, body) {
+    if (err) {
+        throw err;
+    }
+    $ = cheerio.load(body);
+    
+    $('tr').each(function (i, item) {
+
+        var urlSplit = $(this).find('.avatarlink').attr('href').split('/');
+        var author = {
+            username: urlSplit[urlSplit.length - 1],
+            url: $(this).find('.avatarlink').attr('href'),
+            imageUrl: $(this).find('.avatar').attr('src')};
+        var date = $(this).find('.date').text();
+        var message = $(this).find('.commit').text();
+        var url = $(this).find('.commit').attr('href');
+
+        var commit = {
+            author: author,
+            date: date,
+            message: message,
+            url: url
+        }
+        data[i] = commit;
+
+        
+    });
+    console.log("// Pulled %d commits", data.length);
+
+    var jsonData = JSON.stringify(data);
+
+
+    my_http.createServer(function(request,response){  
+        console.log("%s - %s %s", new Date(), request.method, request.url);  
+        if (request.url === '/githup') {
+            response.writeHeader(200, 
+                {"Content-Type": "application/json","Access-Control-Allow-Origin": "*"});  
+            response.write(jsonData);  
+            response.end(); 
+        } 
+        else {
+            response.writeHeader(404, {"Content-Type": "text/plain"});
+            response.write("404 Not found");
+            response.end();
+        } 
+    }).listen(9080);  
+    console.log("Server Running on 9080, access API at /githup"); 
+});
+
